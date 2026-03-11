787a263ccb0f83ed74d6f03dec904ff83b43acce
Jun <junhee726@naver.com>
2026-03-11 22:31:43 +0900

    Title: 특정 원소들의 합, Time: 54ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260311/특정 원소들의 합/sum-of-specific-elements.py
@@ -0,0 +1,10 @@
+arr = [list(map(int, input().split())) for _ in range(4)]
+
+sum = 0
+for i in range(4):
+    for j in range(4):
+        sum += arr[i][j]
+        if i == j:
+            break
+            
+print(sum)
