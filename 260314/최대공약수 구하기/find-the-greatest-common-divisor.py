530f5e6a52caf3ded2df77c67b93a41119320a3a
Jun <junhee726@naver.com>
2026-03-14 23:51:24 +0900

    Title: 최대공약수 구하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260314/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 260314/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -4,13 +4,13 @@ n, m = map(int, input().split())
 def great_com_div(n, m):
     result = 0
     if n < m:
-        for i in range(1, n):
+        for i in range(1, n+1):
             if n % i == 0 and m % i == 0:
                 result = i
         print(result)
         
     elif n > m:
-        for i in range(1, m):
+        for i in range(1, m+1):
             if n % i == 0 and m % i == 0:
                 result = i
         print(result)
