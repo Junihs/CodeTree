c59f75d9f8c77aced7959114c3045476142d1053
Jun <junhee726@naver.com>
2026-03-20 19:54:16 +0900

    Title: 요일 맞추기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260320/요일 맞추기/guess-day-of-week.py
+++ 260320/요일 맞추기/guess-day-of-week.py
@@ -1,13 +1,18 @@
-m1, d1, m2, d2 = map(int, input().split())
-
-# Please write your code here.
 # 30일인 달 -> 달 변경시 30일 추가
 # 31일인 달 -> 달 변경시 31일 추가
 # m1, m2 바교에서 큰 수의 -1까지
+# 역방향 처리
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+
 m_30 = [4, 6, 9, 11]
 m_31 = [1, 3, 5, 7, 8, 10, 12]
 
-sign 
+sign = 1
+if (m1, d1) > (m2, d2):
+    m1, d1, m2, d2 = m2, d2, m1, d1
+    sign = -1 
 
 days = 0
 while True:
@@ -23,20 +28,5 @@ while True:
         days += 28
     m1 += 1
 
-def dow(d):
-    if d % 7 == 0:
-        print("Mon")
-    elif d % 7 == 1:
-        print("Tue")
-    elif d % 7 == 2:
-        print("Wed")
-    elif d % 7 == 3:
-        print("Thu")
-    elif d % 7 == 4:
-        print("Fri")
-    elif d % 7 == 5:
-        print("Sat")
-    else:
-        print("Sun")
-
-dow(days)
+dow = ["Mon", "Tue", "wed", "Thu", "Fri", "Sat", "Sun"]
+print(dow[(sign * days) % 7])
